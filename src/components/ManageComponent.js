e5c796546dc2b67d29012a6092c99fdfdc5c6d8a
LBQTrung <lebaquoctrung@gmail.com>
2023-12-19 22:25:22 +0700

    7. Code UI for Manage Component without animation

--- src/components/ManageComponent.js
+++ src/components/ManageComponent.js
@@ -5,6 +5,7 @@ import database from "./firebase"
 
 const ManageComponent = () => {
     const [data, setData] = useState(null);
+    const [counter, setCounter] = useState(0)
 
 
     useEffect(() => {
@@ -25,15 +26,57 @@ const ManageComponent = () => {
         };
 
         fetchData();
+
     }, []); // Chạy một lần sau khi component được render
 
+
+    const calculateDashOffset = (temperature) => {
+        const maxCounter = 50;
+        const percentage = (temperature / maxCounter) * 100;
+        const dashOffset = 472 - (472 * percentage) / 100;
+        return dashOffset;
+    };
+
+
     return (
-        <div className='measure'>
-            <h3 className='measure-text'>Temperature</h3>
-            <div className='measure-circle'>
+
+        <div className="measure">
+            <div className="measure-cirle">
+                <div className="measure-circle-value">
+                    <div className="measure-value">{data + "°C"}</div>
+                </div>
             </div>
-            <span className='measure-value'>{data}</span>
+
+            <svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="160px" height="160px">
+                <defs>
+                    <linearGradient id="GradientColor">
+                        <stop offset="0%" stopColor="#e91e63" />
+                        <stop offset="100%" stopColor="#FF90BC" />
+                    </linearGradient>
+                </defs>
+                <circle
+                    cx="80"
+                    cy="80"
+                    r="70"
+                    strokeLinecap="round"
+                    style={{
+                        fill: 'none',
+                        stroke: 'url(#GradientColor)',
+                        strokeWidth: '20px',
+                        strokeDasharray: '472',
+                        strokeDashoffset: calculateDashOffset(data),
+                        transformOrigin: 'center',
+                        transform: 'rotate(-90deg)',
+                        animation: 'anim 2s linear', // Disable CSS animation
+                    }} />
+            </svg>
         </div>
+        // <div className='measure'>
+        //     <h3 className='measure-text'>Temperature</h3>
+        //     <div className='measure-circle'>
+        //     </div>
+        //     <span className='measure-value'>{data}</span>
+        // </div>
     )
 }
 
